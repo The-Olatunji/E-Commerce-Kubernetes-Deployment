437838546038380474d110a3f6f6932ee0080354
The-Olatunji <olatunjitope02@gmail.com>
2024-08-20 09:37:34 +0100

    Pipeline

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -37,19 +37,27 @@ variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
 variable "private_subnet_1_cidr" {
- default = "10.0.1.0/24"
+  description = "value of private subnet 1 cidr"
+  type = string
+  default = "10.0.1.0/24"
 }
 
 variable "private_subnet_2_cidr" {
- default = "10.0.2.0/24"
+  description = "value of private subnet 2 cidr"
+  type = string
+  default = "10.0.2.0/24"
 }
 
 variable "private_subnet_3_cidr" {
- default = "10.0.3.0/24"
+  description = "value of private subnet 3 cidr"
+  type = string
+  default = "10.0.3.0/24"
 }
 
 variable "public_subnet_1_cidr" {
- default = "10.0.4.0/24"
+  description = "value of public subnet 1 cidr"
+  type = string
+  default = "10.0.4.0/24"
 }
 
 variable "public_subnet_2_cidr" {
@@ -57,10 +65,14 @@ variable "public_subnet_2_cidr" {
 }
 
 variable "public_subnet_3_cidr" {
- default = "10.0.6.0/24"
+  description = "value of public subnet 3 cidr"
+  type = string
+  default = "10.0.6.0/24"
 }
 
 variable "ami_type" {
+  description = "value of ami type"
+  type = string
   default = "AL2_x86_64"
 }
 
