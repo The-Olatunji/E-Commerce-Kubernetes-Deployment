722d26028ec5a4b92356902f0b9d43dfe0fdc9b2
The-Olatunji <olatunjitope02@gmail.com>
2024-08-21 00:39:03 +0100

    Sock-shop

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "tophe-terraform-statefile"
-#     key = "terraform.tfstate"
-#     region = "us-east-1"
-#     # dynamodb_table = "terraform-lock" 
-#     encrypt        = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "tophe-terraform-statefile"
+    key = "terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-lock" 
+    encrypt        = true
+  }
+}
--- Terraform/s3.tf
+++ Terraform/s3.tf
@@ -1,24 +1,24 @@
 
-# Create the S3 bucket for storing Terraform state
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "tophe-terraform-statefile"
-  lifecycle {
-    prevent_destroy = true
-  }
-  versioning {
-    enabled = false
-  }
+# # Create the S3 bucket for storing Terraform state
+# resource "aws_s3_bucket" "terraform_state" {
+#   bucket = "tophe-terraform-statefile"
+#   # lifecycle {
+#   #   prevent_destroy = true
+#   # }
+#   # versioning {
+#   #   enabled = true
+#   # }
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
+#   server_side_encryption_configuration {
+#     rule {
+#       apply_server_side_encryption_by_default {
+#         sse_algorithm = "AES256"
+#       }
+#     }
+#   }
 
-  tags = {
-    Name        = var.bucket_name
-    Environment = "Dev"
-  }
-}
+#   tags = {
+#     Name        = var.bucket_name
+#     Environment = "Dev"
+#   }
+# }
