fe61f771269701b70bf8c6d207ba288981716411
The-Olatunji <olatunjitope02@gmail.com>
2024-08-21 01:00:12 +0100

    Sock-shop

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket = "tophe-terraform-statefile"
     key = "terraform.tfstate"
     region = "us-east-1"
-    dynamodb_table = "terraform-lock" 
+  # dynamodb_table = "terraform-lock" 
     encrypt        = true
   }
 }
